4c7c396984d3c853246938e3023ef0c714b4ecd4
spacebarley <spacebarley@gmail.com>
2017-05-11 01:40:19 +0900

    Mail warning

--- proj/client/components/NewMail.vue
+++ proj/client/components/NewMail.vue
@@ -14,22 +14,28 @@
               <div class="form-group">
                 <label class="col-md-2" for="to">To</label>
                 <div class="col-md-10">
-                  <input type="text" id="to" class="form-control" placeholder="to" />
+                  <input type="text" id="to" class="form-control" placeholder="To" v-model="to"/>
                 </div>
               </div>
               <div class="form-group">
                 <label class="col-md-2" for="title">Title</label>
                 <div class="col-md-10">
-                  <input type="text" id="title" class="form-control" placeholder="title" />
+                  <input type="text" id="title" class="form-control" placeholder="Title" v-model="title"/>
                 </div>
               </div>
-              <textarea type="text" class="form-control" placeholder="Type here..."></textarea>
-              <input type="file" class="form-control" />
+              <textarea type="text" id="body" class="form-control" placeholder="Type Here" v-model="body"></textarea>
+              <input type="file" class="form-control" @change="onFileChange"/>
             </form>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary">Send</button>
+          <div class="alert alert-danger alert-dismissable fade show" v-if="alert">
+            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            <strong>Danger! </strong> {{ alertMessage }}
           </div>
+          <div class="modal-footer">
+            <button type="button" id="sendButton" class="btn btn-primary" aria-label="Close" data-dismiss="alert" @click.prevent.stop="checkAttached">Send</button>
+          </div>
+
+
         </div>
       </div>
     </div>
@@ -37,18 +43,85 @@
 </template>
 <script>
   import { mapActions } from 'vuex'
-
+  import firebase, { getMails } from '../firebase'
   export default {
     components: {
+    },
+    data () {
+      return {
+        body: '',
+        alert: false,
+        alertMessage: '',
+        attached: false,
+        from: '',
+        to: '',
+        title: '',
+        // attachment
+      }
     },
     methods: {
       onClickClose () {
         this.closeNewMail ();
       },
+      onFileChange (e) {
+        this.attached = true;
+      },
+      checkAttached (e) {
+        const bannedWords = ["첨부", "attachment", "attach"]
+        if (!this.attached){
+          for (let i=0;i<bannedWords.length;i++){
+            if (this.body.includes(bannedWords[i])) {          
+              this.alert=true;
+              this.alertMessage="The word \""+bannedWords[i]+"\" included on your text, but you didn't attached any file."
+              console.log("WOrdked")
+              break
+            }        
+          }
+        }
+      },
       ...mapActions ([
         'closeNewMail',
+        'setMails',
       ]),
     },
+//     async pushMails () {
+//       const mails = await getMails()
+//       var newKey = mails.length
+//       var newMail = {}
+//       newMail["mailId"] = "random"
+//       newMail["from"] = 
+// /*
+//     {
+//       "mailid": 12,
+//       "from": {
+//         "name": "Juho Kim",
+//         "address": "juho.kim@kaist.ac.kr"
+//       },
+//       "to": {
+//         "address": "changjej@kaist.ac.kr"
+//       },
+//       "title": "[DP2] Notice on DP2 presentation",
+//       "content": "Blah",
+//       "sent": "2017-05-09 19:20:51",
+//       "attachments": {
+//         0:{
+//           "filename": "notice.pdf",
+//           "link": "https://firebasestorage.googleapis.com/v0/b/cs374-32b99.appspot.com/o/pintos.pdf?alt=media&token=c9a185e4-d1bd-4f62-ae4b-75bdb52c01d7",
+          
+//         },
+//        1: {
+//           "filename": "report.pdf",
+//           "link": "https://firebasestorage.googleapis.com/v0/b/cs374-32b99.appspot.com/o/pintos.pdf?alt=media&token=c9a185e4-d1bd-4f62-ae4b-75bdb52c01d7",
+          
+//         }
+//         }
+//     },
+//     */
+
+//       let mailsRef = firebase.database().ref('/mails/')
+//       mailsRef.update({newKey:newMail});
+
+//     },
   }
 </script>
 
